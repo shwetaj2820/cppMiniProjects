b168d2447dbd751302245fa25abd3d70e15a48d3
Shweta Joshi <93914795+shwetaj2820@users.noreply.github.com>
2023-01-04 23:39:21 +0530

    Obtain current system time

--- digital_clock.cpp
+++ digital_clock.cpp
@@ -3,3 +3,10 @@
 #include<windows.h>
 using namespace std;
 
+int main(){
+  time_t t = time(NULL);  //time() method in c++ is used. object type: current time as a value
+  tm *timePtr = localtime(&t); //localtime() method is used to convert current time to identifier
+  
+  
+  
+}
