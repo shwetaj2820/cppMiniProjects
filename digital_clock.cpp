e73a3d6ed9dcd1a09d111a08eca627668739f812
Shweta Joshi <93914795+shwetaj2820@users.noreply.github.com>
2023-01-05 18:24:53 +0530

    Utilize struct attributes

--- digital_clock.cpp
+++ digital_clock.cpp
@@ -7,6 +7,11 @@ int main(){
   time_t t = time(NULL);  //time() method in c++ is used. object type: current time as a value
   tm *timePtr = localtime(&t); //localtime() method is used to convert current time to identifier
   
+//   storing the local time in variables from the timePtr structure
+  int seconds = (timePtr->tm_sec);
+  int minutes = (timePtr->tm_min);
+  int hours = (timePtr->hour);
   
   
+  return 0;
 }
