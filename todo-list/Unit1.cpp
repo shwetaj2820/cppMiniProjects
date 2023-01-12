4b66d079422f356e025ec4b3f73f37708bc70069
Shweta Joshi <93914795+shwetaj2820@users.noreply.github.com>
2023-01-12 16:08:35 +0530

    Create C++ source code

---
+++ todo-list/Unit1.cpp
@@ -0,0 +1,32 @@
+//---------------------------------------------------------------------------
+
+#include <fmx.h>
+#pragma hdrstop
+
+#include "Unit1.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.fmx"
+TForm1 *Form1;
+//---------------------------------------------------------------------------
+__fastcall TForm1::TForm1(TComponent* Owner)
+	: TForm(Owner)
+{
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::AddButtonClick(TObject *Sender)
+{
+	String txt = AddTask->Text;
+	TaskList->Items->Add(txt);
+    AddTask->Text="";
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::TaskListItemClick(TCustomListBox * const Sender, TListBoxItem * const Item)
+
+{
+	int index = Item->Index;
+    TaskList->Items->Delete(index);
+}
+//---------------------------------------------------------------------------
+
